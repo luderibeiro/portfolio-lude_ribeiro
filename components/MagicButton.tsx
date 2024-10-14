85b51fc9c91d5096cf4b0f458c82f0e44002cbc3
Lude Ribeiro <ludeyuri07@gmail.com>
2024-10-14 13:59:00 -0300

    Update MagicButton.

--- components/MagicButton.tsx
+++ components/MagicButton.tsx
@@ -15,20 +15,16 @@ const MagicButton = ({
 }) => {
     return (
         <button
-            className="relative inline-flex h-12 w-full md:w-60 md:mt-10 rounded-lg p-[1px] focus:outline-none"
+            className="relative inline-flex h-12 w-full md:w-60 md:mt-10 rounded-lg p-[1px] focus:outline-none overflow-hidden group"
             onClick={handleClick}
         >
+            <span className="absolute inset-0 bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)] transition-transform duration-6000 ease-linear animate-spin-slow -z-10 opacity-90 " />
             <span
-                className="absolute inset-0 -z-10 animate-[spin_2s_linear_infinite] 
-                bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]"
+                className={`relative inline-flex h-full w-full items-center justify-center rounded-lg 
+                bg-slate-950 px-5 text-sm font-medium text-white gap-2 ${otherClasses}`}
                 style={{
-                    WebkitMaskImage: '-webkit-radial-gradient(circle, white, black)',
-                    maskImage: 'radial-gradient(circle, white, black)',
+                    background: "rgba(0, 0, 0, 1)",
                 }}
-            />
-            <span
-                className={`inline-flex h-full w-full items-center justify-center rounded-lg 
-                bg-slate-950/90 px-5 text-sm font-medium text-white gap-2 ${otherClasses}`}
             >
                 {position === "left" && icon}
                 {title}
