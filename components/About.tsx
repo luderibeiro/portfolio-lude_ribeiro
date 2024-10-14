54a0b5af6d106d180583cc763fa3b2768a63ed26
Lude Ribeiro <ludeyuri07@gmail.com>
2024-10-14 13:29:35 -0300

    Update About page and text.

--- components/About.tsx
+++ components/About.tsx
@@ -6,37 +6,44 @@ export const About = () => {
         <div id="about" className="pb-20 pt-36">
             <div className="flex flex-col items-center justify-center">
                 <TextGenerateEffect
-                    className="text-center text-[40px] md:text5xl lg:text-6xl"
+                    className="text-center text-[40px] md:text-5xl lg:text-6xl"
                     words="Mais sobre mim"
                 />
-                <div className="flex flex-col w-full py-3 px-5 pr-10 gap-20 align-middle justify-center items-center rounded-md shadow-md shadow-cyan-900">
+                <div className="flex flex-col md:flex-row w-full py-3 px-5 pr-10 gap-10 md:gap-20 items-center rounded-md shadow-md shadow-cyan-900">
                     <img
                         src="/profile.jpg"
                         width={480}
                         height={480}
                         alt="Foto de perfil"
+                        className="w-64 h-64 md:w-96 md:h-96 rounded-lg"
                     />
-                    <p className="font-light font-sans text-justify text-md sm:text-sm md:text-sm lg:text-lg pb-5 right-0 px-10">
-                        Meu nome é Lude Ribeiro, sou um desenvolvedor web
-                        FullStack com foco em tecnologias modernas e práticas de
-                        desenvolvimento de software. Atualmente, estou me
+                    <p className="font-light font-sans text-justify text-md sm:text-sm md:text-sm lg:text-lg px-5 md:px-10">
+                        Olá! Meu nome é Lude Ribeiro, e sou um desenvolvedor
+                        FullStack apaixonado por tecnologias modernas e práticas
+                        eficientes de desenvolvimento de software. Estou me
                         formando em Engenharia de Software na Universidade de
-                        Brasília.
+                        Brasília, e desde o início da minha jornada, tenho
+                        buscado sempre por aprimoramento e inovação.
                         <br />
                         <br />
-                        Tenho mais de 3 anos de experiência em desenvolvimento
-                        web e mobile, trabalhando com soluções de alta
-                        performance e escaláveis. Meu objetivo é sempre
-                        aprender, ensinar e compartilhar conhecimento com
-                        pessoas que estão começando ou já estão no mercado de
-                        trabalho. Estou sempre em busca de novos desafios e
-                        oportunidades para crescer profissionalmente. Aberto
-                        para contratações formais, projetos e freelances.
+                        Com mais de 3 anos de experiência em desenvolvimento web
+                        e mobile, estou focado em construir soluções de alta
+                        performance e escalabilidade. Adoro aprender e
+                        compartilhar conhecimento, seja com pessoas que estão
+                        começando ou com profissionais experientes. Acredito no
+                        potencial da tecnologia para transformar e melhorar a
+                        vida das pessoas.
                         <br />
                         <br />
-                        Minhas principais habilidades são: Python, Django,
-                        JavaScrip, React, Next.Js, TypeScript, Docker, Git, AWS,
-                        entre outras.
+                        Atualmente, estou em busca de novos desafios e
+                        oportunidades para crescer profissionalmente, e estou
+                        disponível para projetos de contratação formal,
+                        consultoria e freelances.
+                        <br />
+                        <br />
+                        Minhas principais habilidades incluem: Python, Django,
+                        JavaScript, React, Next.js, TypeScript, Docker, Git, e
+                        AWS, entre outras.
                     </p>
                 </div>
             </div>
